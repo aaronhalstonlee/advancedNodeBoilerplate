aa2491ae5bf9261cbf5965f4d4bd356ef7c5355c
aaron lee <aaronhalstonlee@gmail.com>
2021-03-27 10:13:23 -0700

    add register user

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ myDB(async (client) => {
     res.render('pug', {
       title: 'Connected to database', 
       message: 'Please login',
-      showLogin: true
+      showLogin: true,
+      showRegistration: true
     });
   });
 
@@ -52,6 +53,29 @@ myDB(async (client) => {
   app.route('/logout').get((req, res) => {
     req.logout();
     res.redirect('/');
+  });
+
+  app.route('/register').post((req, res, next) => {
+    myDataBase.findOne({username: req.body.username}, (err, user) => {
+      if(err){ 
+        next(err) 
+      } else if(user){
+        res.redirect('/');
+      } else {
+        myDataBase.insertOne({username: req.body.username, password: req.body.password }, (err, doc) => {
+          if(err) {
+            console.log('could not insert user to database')
+            console.log(err);
+            res.redirect('/')
+          } else {
+            next(null, doc.ops[0]);
+          }
+        });
+      }
+    });
+  },
+  passport.authenticate('local', {failureRedirect: '/' }), (req, res, next) => {
+    res.redirect('/profile');
   });
 
   app.use((req, res, next) => {
